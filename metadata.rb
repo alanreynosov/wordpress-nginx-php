ce7264d094fc27f894de6c8a5df79f92bb6a93b1
Alan <alan@paradojo.com>
2016-04-16 20:09:49 -0500

    Create metadata.rb

---
+++ metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Alan"
+maintainer_email "alan@paradojo.co,"
+license          "All rights reserved"
+description      "Deploy Wordpres Running nginx"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
